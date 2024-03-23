9026fa720536224d5a4968eb73d47423f09c5b46
Sidra-Noman <147878619+Sidra-Noman@users.noreply.github.com>
2024-03-24 03:23:34 +0500

    Add files via upload

---
+++ case sensitive/case.js
@@ -0,0 +1,13 @@
+// camel case
+// let myHouse : string = "Beautiful";
+// console.log(myHouse)
+// snake_case
+// let i_am_perfect : boolean = true;
+// console.log(i_am_perfect)
+// pascal case
+// let SiblingNumber : number = 4;
+// console.log(SiblingNumber)
+var myCity = 'Karachi';
+var is_big = true;
+var NoOfProvinces = 4;
+console.log(myCity, is_big, NoOfProvinces);
---
+++ case sensitive/case.ts
@@ -0,0 +1,17 @@
+// camel case
+// let myHouse : string = "Beautiful";
+// console.log(myHouse)
+
+// snake_case
+
+// let i_am_perfect : boolean = true;
+// console.log(i_am_perfect)
+
+// pascal case
+// let SiblingNumber : number = 4;
+// console.log(SiblingNumber)
+
+let myCity : string = 'Karachi';
+let is_big : boolean = true;
+let NoOfProvinces : number = 4;
+console.log(myCity,is_big, NoOfProvinces)
